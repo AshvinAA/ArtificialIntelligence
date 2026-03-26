99106327d2f817c6e01449e127a5c96087cacf38
AshvinAA <ashvin.afroz@gmail.com>
2026-03-26 23:41:14 +0600

    A* problem1 spotted

---
+++ GeneticAlgorithms/Problem1.py
@@ -0,0 +1,20 @@
+BLOCKS = [
+    ["ALU", 5, 5],
+    ["Cache", 7, 4],
+    ["Control␣Unit", 4, 4],
+    ["Register␣File", 6, 6],
+    ["Decoder", 5, 3],
+    ["Floating␣Unit", 5, 5]
+]
+
+ALPHA=1000
+BETA = 2
+GAMMA = 1
+
+ind = [(9,3), (12 ,15), (13 ,16),
+(1,13), (4,15), (9,6)]
+
+def overlap_count(self,ind):
+    pass
+
+
